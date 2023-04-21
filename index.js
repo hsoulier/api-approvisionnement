b2a1412b78249cea85996460afcf66a55d4b9bc1
AlanDufail <alan.dufail@mail-ecv.fr>
2023-04-21 10:42:30 +0200

    update 2

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Add Express
-const { default: axios } = require("axios")
-const express = require("express")
+const { default: axios } = require("axios");
+const express = require("express");
 
 // Initialize Express
-const app = express()
+const app = express();
 
-const productList = new Map()
+const productList = new Map();
 
 // Create GET request
 app.get("/api/ping", (req, res) => {
-  res.send("PONG")
-})
+  res.send("PONG");
+});
 
 app.post("/api/supply", async (req, res) => {
   try {
-    const { supplyId, products } = req.body
+    const { supplyId, products } = req.body;
     if (!supplyId || !products) {
-      throw new Error("Missing supplyId or products")
+      throw new Error("Missing supplyId or products");
     }
     if (!Array.isArray(products)) {
-      throw new Error("Products must be an array")
+      throw new Error("Products must be an array");
     }
     for (const product of products) {
       if (
@@ -29,44 +29,48 @@ app.post("/api/supply", async (req, res) => {
         !product.purchasePricePerUnit ||
         !product.quantity
       ) {
-        throw new Error("Missing product data")
+        throw new Error("Missing product data");
       }
-      if (productList.has(product.ean)) {
-        productList.set(product.ean, {
-          ...productList.get(product.ean),
-          quantity: productList.get(product.ean).quantity + product.quantity,
-        })
-      } else {
-        productList.set(product.ean, {
-          ...productList.get(product.ean),
-          price: product.purchasePricePerUnit,
-          categories: [],
-        })
-      }
-      await axios.post(`/api/stock/${product.ean}/movement`, {
-        productId: product.ean,
-        quantity: product.quantity,
-        status: "Supply",
-      })
-    }
+      const res = await axios.get(
+        "http://microservices.tp.rjqu8633.odns.fr/api/products"
+      );
 
-    res.status(204).end()
+      if (!res.data.find((p) => p.ean === product.ean)) {
+        await axios.post(
+          "http://microservices.tp.rjqu8633.odns.fr/api/products",
+          {
+            ean: product.ean,
+            name: product.name,
+            description: product.description,
+            categories: [],
+            price: product.purchasePricePerUnit,
+          }
+        );
+
+        await axios.post(`/api/stock/${product.ean}/movement`, {
+          productId: product.ean,
+          quantity: product.quantity,
+          status: "Supply",
+        });
+      }
+    }
+    res.status(204).end();
   } catch (error) {
-    res.status(400).send(error.message)
+    res.status(400).send(error.message);
   }
-})
+});
 
 app.get("/api/supply/summary", async (req, res) => {
   try {
-    res.status(200).end()
+    res.status(200).end();
   } catch (error) {
-    res.status(400).send(error.message)
+    res.status(400).send(error.message);
   }
-})
+});
 
 // Initialize server
-app.listen(5000, () => {
-  console.log("Running on port 5000.")
-})
+app.listen(5001, () => {
+  console.log("Running on port 5001.");
+});
 
-module.exports = app
+module.exports = app;
