8c6ce6d4d02b318668440d9c3190994a25da8655
hsoulier <hi.soulier@gmail.com>
2023-04-21 10:11:00 +0200

    ✨ Feat: Issue #1

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 // Add Express
-const express = require("express");
+const { default: axios } = require("axios")
+const express = require("express")
 
 // Initialize Express
-const app = express();
+const app = express()
 
 // Create GET request
 app.get("/api/ping", (req, res) => {
-  res.send("PONG");
-});
+  res.send("PONG")
+})
+
+app.post("/api/supply", async (req, res) => {
+  try {
+    const { supplyId, products } = req.body
+    if (!supplyId || !products) {
+      throw new Error("Missing supplyId or products")
+    }
+    if (!Array.isArray(products)) {
+      throw new Error("Products must be an array")
+    }
+    for (const product of products) {
+      if (
+        !product.ean ||
+        !product.name ||
+        !product.description ||
+        !product.purchasePricePerUnit ||
+        !product.quantity
+      ) {
+        throw new Error("Missing product data")
+      }
+      await axios.post(`/api/stock/${product.ean}/movement`, {
+        productId: product.ean,
+        quantity: product.quantity,
+        status: "Supply",
+      })
+    }
+
+    res.status(204).end()
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+})
 
 // Initialize server
 app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
+  console.log("Running on port 5000.")
+})
 
-module.exports = app;
+module.exports = app
